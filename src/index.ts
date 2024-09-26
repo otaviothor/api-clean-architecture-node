62f0aba1dc6b7562bb02e12c7ab47be31e30f532
Otávio Silva <otaviosilva2632@gmail.com>
2024-09-26 14:20:52 -0300

    feat: remove unused imports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { errorHandler } from "./interface/middleware/errorHandler";
 import { bookRoutes } from "./interface/routes/bookRoutes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", bookRoutes);
+app.use(errorHandler);
 
 const PORT = 3000;
 app.listen(PORT, () => {
